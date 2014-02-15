6d918432a4f5c7823baf6b10b98058c99742b1e7
Matthew Loberg <loberg.matt@gmail.com>
2014-02-15 16:32:31 -0600

    Update python::version
    Version now installs the class instead of a provider.
    
    Added the ability to use custom environment settings.
    
    Use Hiera to set some defaults such as any environment settings. Also use aliases (2.7 -> 2.7.6).

--- manifests/version.pp
+++ manifests/version.pp
@@ -4,15 +4,89 @@
 #
 #   python::version { '2.7.3' }
 #
+
 define python::version(
-  $ensure  = present,
-  $version = $title,
+  $ensure  = 'installed',
+  $env     = {},
+  $version = $name,
 ) {
   require python
 
-  python { $version:
-    ensure     => $ensure,
-    pyenv_root => $python::pyenv_root,
-    user       => $python::pyenv_user,
+  case $version {
+    'jython': { require java }
+    default: { }
+  }
+
+  $alias_hash = hiera_hash('python::version::alias', {})
+  if has_key($alias_hash, $version) {
+    $taget = $alias_hash[$version]
+
+    file { "${python::pyenv_root}/versions/${version}":
+      ensure => symlink,
+      force  => true,
+      target => "${python::pyenv_root}/versions/${target}"
+    }
+
+    ensure_resource('python::version', $target)
+  } else {
+    case $::osfamily {
+      'Darwin': {
+        require xquartz
+        include homebrew::config
+        include boxen::config
+
+        $os_env = {
+          'CFLAGS'  => "-I${homebrew::config::installdir}/include -I/opt/X11/include",
+          'LDFLAGS' => "-L${homebrew::config::installdir}/lib -L/opt/X11/lib",
+        }
+      }
+
+      default: {
+        $os_env = {}
+      }
+    }
+
+    $dest = "${python::pyenv_root}/versions/${version}"
+
+    if $ensure == 'absent' {
+      file { $dest:
+        ensure => absent,
+        force  => true,
+      }
+    } else {
+      $default_env = {
+        'CC'         => '/usr/bin/cc',
+        'PYENV_ROOT' => $python::pyenv_root,
+      }
+
+      $hiera_data = hiera_hash('python::version::env', {})
+      if has_key($hiera_data, $version) {
+        $hiera_env = $hiera_data[$version]
+      } else {
+        $hiera_env = {}
+      }
+
+      $final_env = merge(merge(merge($default_env, $os_env), $hiera_env), $env)
+
+      if has_key($final_env, 'CC') {
+        case $final_env['CC'] {
+          /gcc/: { require gcc }
+          default: { }
+        }
+      }
+
+      exec { "python-install-${version}":
+        command  => "${python::pyenv_root}/bin/pyenv install ${version}",
+        cwd      => "${python::pyenv_root}/versions",
+        provider => 'shell',
+        timeout  => 0,
+        creates  => $dest,
+        user     => $python::user,
+      }
+
+      Exec["python-install-${version}"] {
+        environment +> sort(join_keys_to_values($final_env, '='))
+      }
+    }
   }
 }
--- spec/defines/python_version_spec.rb
+++ spec/defines/python_version_spec.rb
@@ -2,25 +2,64 @@ require "spec_helper"
 
 describe "python::version" do
   let(:facts) { default_test_facts }
-  let(:title) { "2.7.3" }
+  let(:title) { "2.7.6" }
 
-  it do
-    should include_class("python")
+  context "ensure => present" do
+    context "default params" do
+      it do
+        should include_class("python")
 
-    should contain_python("2.7.3").with({
-      :ensure  => "present"
-    })
+        should contain_exec("python-install-2.7.6").with({
+          :command  => "/test/boxen/pyenv/bin/pyenv install 2.7.6",
+          :cwd      => '/test/boxen/pyenv/versions',
+          :provider => 'shell',
+          :timeout  => 0,
+          :creates  => '/test/boxen/pyenv/versions/2.7.6'
+        })
+      end
+    end
+
+    context "when env is default" do
+      it do
+        should contain_exec("python-install-2.7.6").with_environment([
+          "CC=/usr/bin/cc",
+          "CFLAGS=-I/test/boxen/homebrew/include -I/opt/X11/include",
+          "LDFLAGS=-L/test/boxen/homebrew/lib -L/opt/X11/lib",
+          "PYENV_ROOT=/test/boxen/pyenv"
+        ])
+      end
+    end
+
+    context "when env is not nil" do
+      let(:params) do
+        {
+          :env => {'SOME_VAR' => 'foobar'}
+        }
+      end
+
+      it do
+        should contain_exec("python-install-2.7.6").with_environment([
+          "CC=/usr/bin/cc",
+          "CFLAGS=-I/test/boxen/homebrew/include -I/opt/X11/include",
+          "LDFLAGS=-L/test/boxen/homebrew/lib -L/opt/X11/lib",
+          "PYENV_ROOT=/test/boxen/pyenv",
+          "SOME_VAR=foobar",
+        ])
+      end
+    end
   end
 
-  context "non-default parameter values" do
+  context "ensure => absent" do
     let(:params) do
-      { :ensure => "absent", :version => "2.6.8" }
+      {
+        :ensure => 'absent'
+      }
     end
 
     it do
-      should contain_python("2.6.8").with({
-        :ensure  => "absent",
-        :version => "2.6.8"
+      should contain_file('/test/boxen/pyenv/versions/2.7.6').with({
+        :ensure => 'absent',
+        :force  => true,
       })
     end
   end
