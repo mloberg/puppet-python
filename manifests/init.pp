1ce92d6bf3f574d92cd55b42a24e8c9988e49cc8
Matthew Loberg <mloberg@nerdery.com>
2014-11-26 09:50:12 -0600

    Add hiera data support
    Added hiera data support so that users could override build options, where pyenv is installed and what version is installed.
    
    Moved installing pyenv to it's own class so that in hiera we can use python::pyenv.

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,51 +5,27 @@
 #   include python
 #
 class python(
-  $pyenv_plugins    = {},
-  $pyenv_version    = $python::params::pyenv_version,
-  $pyenv_root       = $python::params::pyenv_root,
-  $user             = $python::params::user,
-) inherits python::params {
-
+  $prefix   = $python::prefix,
+  $user     = $python::user,
+) {
   if $::osfamily == 'Darwin' {
     include boxen::config
+  }
 
-    package { 'readline':
-      ensure => latest,
-    }
+  include python::pyenv
 
-    file { "${boxen::config::envdir}/pyenv.sh":
-      ensure => absent,
-    }
-
+  if $::osfamily == 'Darwin' {
     boxen::env_script { 'pyenv':
       source   => 'puppet:///modules/python/pyenv.sh',
       priority => 'higher'
     }
   }
 
-  repository { $pyenv_root:
-    ensure => $pyenv_version,
-    source => 'yyuu/pyenv',
-    user   => $user,
   }
 
-  file {
-    [
-      "${pyenv_root}/pyenv.d",
-      "${pyenv_root}/pyenv.d/install",
-      "${pyenv_root}/shims",
-      "${pyenv_root}/versions",
-    ]:
-      ensure  => directory,
-      require => Repository[$pyenv_root],
-  }
+  Class['python::pyenv'] ->
+    Python::Version <| |> ->
+    Python::Plugin <| |>
 
-  $_real_pyenv_plugins = merge($python::params::pyenv_plugins, $pyenv_plugins)
-  create_resources('python::plugin', $_real_pyenv_plugins)
 
-  Repository[$pyenv_root] ->
-    File <| tag == 'python_plugin_config' |> ->
-    Python::Plugin <| |> ->
-    Python::Version <| |>
 }
